06d66af641f73827a72e7034862dc101f1662540
Paige Wiener <snowplushie@yahoo.com>
2019-04-05 01:44:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,9 @@ class Triangle
   
   def kind
     validate_triangle
-    if a==b && b==c
+    if a == b && b == c
       :equilateral
-    elsif a==b || b==c || a==c
+    elsif a == b || b = =c || a == c
       :isosceles
     else
       :scalene
@@ -20,7 +20,7 @@ class Triangle
   
   def validate_triangle
     real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
-    [a, b, c].each {|t| real_triangle << false is t <= 0}
+    [a, b, c].each {|s| real_triangle << false is s <= 0}
     raise TriangleError if real_triangle.include?(false)
   end
   
