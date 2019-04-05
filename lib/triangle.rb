e4428229713d3454841126f85fe01547676657d1
Paige Wiener <snowplushie@yahoo.com>
2019-04-05 01:36:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,8 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+  attributes=[]
+  
+  def initialize(attributes)
+    attributes.each {|key, value| self.send (("#{key}="), value)}
+  end
 end
